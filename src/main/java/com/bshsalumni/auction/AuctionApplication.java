d95aa5dfe5806621e81ff0b1f4a17072f04be912
Swastik Dash <swastikdash@Swastiks-MacBook-Pro.local>
2023-02-28 13:15:34 +0530

    added liquibase support and updated server port

--- src/main/java/com/bshsalumni/auction/AuctionApplication.java
+++ src/main/java/com/bshsalumni/auction/AuctionApplication.java
@@ -1,13 +1,23 @@
 package com.bshsalumni.auction;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@Slf4j
+@EnableWebMvc
 @SpringBootApplication
-public class AuctionApplication {
+public class AuctionApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuctionApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		log.info("Auction application version {} started successfully", 1.0);
+		log.warn("IMAGE URL TO BE ADDED FOR PLAYERS");
+	}
 }
