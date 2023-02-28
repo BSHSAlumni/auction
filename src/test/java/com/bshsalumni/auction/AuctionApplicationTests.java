c5603f8fef2a2aeb2b97d3a39557c06339ae3a38
Swastik Dash <swastikdash@Swastiks-MacBook-Pro.local>
2023-02-28 12:35:03 +0530

    initial commit

---
+++ src/test/java/com/bshsalumni/auction/AuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bshsalumni.auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
