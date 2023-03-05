3c7c9540c6a99f7da157c1382fc5626f4590dd37
Swastik Dash <swastikdash@Swastiks-MacBook-Pro.local>
2023-03-05 18:53:15 +0530

    added models

--- src/main/java/com/bshsalumni/auction/model/Team.java
+++ src/main/java/com/bshsalumni/auction/model/Team.java
@@ -1,4 +1,10 @@
 package com.bshsalumni.auction.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
 /*
 
  * Date : 02/03/23
@@ -6,8 +12,26 @@ package com.bshsalumni.auction.model;
  * Author : SWASTIK PREETAM DASH
 
  */
-
+@Getter
+@Setter
+@Entity
+@Table(name = "team_data")
 public class Team {
 
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "logo", nullable = false)
+    private String logo;
+
+    @Column(name = "captain_id", nullable = false)
+    private Integer captainId;
+
+    @Column(name = "wallet_remaining", nullable = false)
+    private Integer walletRemaining;
 }
